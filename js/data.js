b99c041d1a3a7aee3b635f615251883e5bcefab3
joa <jotaquin99g@gmail.com>
2023-09-01 18:46:11 -0300

    Agregando shop, footer y estilos

---
+++ js/data.js
@@ -0,0 +1,64 @@
+const productos = [
+    {
+        nombre: 'Buzo Attack on Titan',
+        precio: 7500,
+        imagen: '../TiendaHisoka/img/img-sinfondo/buzo-attack-on-titan.png'
+    },
+    {
+        nombre: 'Aros Tanjiro Kamado',
+        precio: 900,
+        imagen: '../TiendaHisoka/img/img-sinfondo/aros-tanjiro.png'
+    },
+    {
+        nombre: 'Taza One Piece',
+        precio: 1200,
+        imagen: '../TiendaHisoka/img/img-sinfondo/taza-one-piece.png'
+    },
+    {
+        nombre: 'Almohada Akatsuki',
+        precio: 1500,
+        imagen: '../TiendaHisoka/img/img-sinfondo/almohada-akatsuki.png'
+    },
+    {
+        nombre: 'Llavero Katana',
+        precio: 1100,
+        imagen: '../TiendaHisoka/img/img-sinfondo/llavero-katana.png'
+    },
+    {
+        nombre: 'Buzo Death Note',
+        precio: 7500,
+        imagen: '../TiendaHisoka/img/img-sinfondo/buzo-death-note.png'
+    },
+    {
+        nombre: 'Falda Negra',
+        precio: 4200,
+        imagen: '../TiendaHisoka/img/img-sinfondo/falda-negra.png'
+    },
+    {
+        nombre: 'Remera Akatsuki',
+        precio: 3500,
+        imagen: '../TiendaHisoka/img/img-sinfondo/remera-akatsuki.png'
+    },
+    {
+        nombre: 'Buzo Mario',
+        precio: 7500,
+        imagen: '../TiendaHisoka/img/img-sinfondo/buzo-mario.png'
+    },
+    {
+        nombre: 'Aros Potara Dragon Ball',
+        precio: 1200,
+        imagen: '../TiendaHisoka/img/img-sinfondo/potara-dragon-ball-amarillo.png'
+    },
+    {
+        nombre: 'Buzo Slipknot',
+        precio: 7500,
+        imagen: '../TiendaHisoka/img/img-sinfondo/buzo-slipknot.png'
+    },
+    {
+        nombre: 'Taza Gon',
+        precio: 1400,
+        imagen: '../TiendaHisoka/img/img-sinfondo/taza-gon.png'
+    },
+    
+];
+
---
+++ js/main.js
@@ -0,0 +1,45 @@
+const btnCart = document.querySelector('.container-icon');
+const containerCartProductos = document.querySelector('.container-cart-productos');
+
+btnCart.addEventListener('click', () => {
+    containerCartProductos.classList.toggle('hidden-cart')
+});
+
+// // Obtener elementos del DOM
+// const productoSeccion = document.getElementById('producto');
+
+// // Funcion para crear una card de producto
+// function crearCardProducto(producto) {
+//     const card = document.createElement('div');
+//     card.className = 'card-productos';
+
+//     const image = document.createElement('img');
+//     image.className = 'img-cards-producto';
+//     image.src = producto.imagen;
+//     image.alt = producto.nombre;
+
+//     const productoNombre = document.createElement ('h4');
+//     productoNombre.textContent = producto.nombre;
+//     productoNombre.className = 'nombre-producto';
+
+//     const productoPrecio = document.createElement('p');
+//     productoPrecio.textContent = `Precio: $${producto.precio}`;
+//     productoPrecio.className = 'precio-producto';
+
+//     const agregarAlCarritoButton = document.createElement('button');
+//     agregarAlCarritoButton.className = 'agregar-al-carrito btn-producto';
+//     agregarAlCarritoButton.textContent = 'Agregar al carrito';
+
+//     card.appendChild(image);
+//     card.appendChild(productoNombre);
+//     card.appendChild(productoPrecio);
+//     card.appendChild(agregarAlCarritoButton);
+
+//     return card;
+// }
+
+// // Generar y agregar las cards de productos al DOM
+// productos.forEach(producto => {
+//     const productoCard = crearCardProducto(producto);
+//     productoSeccion.appendChild(productoCard);
+// });
